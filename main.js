ec31b7765b1d8e0b74360f68e50ec190955885a9
8NB8 <8nebojsa@gmail.com>
2023-11-18 09:40:43 +0100

    style and image cover add

--- main.js
+++ main.js
@@ -39,21 +39,22 @@ input.addEventListener("input", async () => {
     div.innerHTML = `<p class="item">${word}</p>`;
     listContainer.appendChild(div);
   });
-
-  jsonData.data["results"].forEach((element) => {
+  
+jsonData.data["results"].forEach((element) => {
     showContainer.innerHTML += `
-        <div class="bg-white">
-            <div class="mb-2">
-            <img class="" src="${
-                element.thumbnail["path"] + "." + element.thumbnail["extension"]
-            }"/></div>
-            <div class="character-name text-black mb-3 text-center">${element.name}</div>
-            <div class="text-center pb-4">
-            <a href="#bookmark" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-gray-400">
+      <div class="bg-white">
+        <div class="mb-2 flex justify-center items-center">
+          <img class="character-image" src="${
+            element.thumbnail["path"] + "." + element.thumbnail["extension"]
+          }"/>
+        </div>
+        <div class="character-name text-black mb-3 text-center">${element.name}</div>
+        <div class="text-center pb-4">
+          <a href="#bookmark" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-gray-400">
             Add to favorite
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16"> <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/> </svg>
-            </a>
-            </div>
-        </div>`;
+          </a>
+        </div>
+      </div>`;
   });
 });
